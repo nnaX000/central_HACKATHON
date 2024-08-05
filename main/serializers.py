f606ee5446d81b3aa5132f4d9c87fb919dd06ce6
suncastle023 <badabada0619@naver.com>
2024-08-05 20:02:25 +0900

    fix: 알람에 프로필 이미지 필드 추가

--- main/serializers.py
+++ main/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
 from main.models import Notification  
 from post.models import Post  
+from user.models import CustomUser
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'content', 'image', 'date_posted', 'author', 'total_likes'] 
+        fields = ['id', 'content', 'image', 'date_posted', 'author', 'total_likes']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['user_id', 'username', 'email', 'photo']
 
 class NotificationSerializer(serializers.ModelSerializer):
     post = PostSerializer(read_only=True)
-    sender = serializers.StringRelatedField()  
+    sender = UserSerializer(read_only=True) 
     recipient = serializers.StringRelatedField() 
 
     class Meta:
